a1b4756a2b61f997cbd8bf1a86baffac10ea3e43
Sebastien REMY <seb.remy@me.com>
2019-11-11 04:18:19 +0100

    Change the text
    with random

--- app/src/main/java/com/sebremy/diceroller/MainActivity.kt
+++ app/src/main/java/com/sebremy/diceroller/MainActivity.kt
@@ -3,6 +3,8 @@ package com.sebremy.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,15 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
     }
 }
